c7e375177740d489609efa75108a023afde2f27f
yong1le <ygle.he@gmail.com>
2023-07-19 23:15:09 -0400

    add views
    general stores activity and 3 fragments for it

---
+++ app/src/main/java/com/example/b07_final_project/MyStoresFragment.java
@@ -0,0 +1,52 @@
+package com.example.b07_final_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MyStoresFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MyStoresFragment extends Fragment {
+
+    public MyStoresFragment() {
+        super(R.layout.fragment_my_stores);
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MyStoresFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+//    public static MyStoresFragment newInstance(String param1, String param2) {
+//        MyStoresFragment fragment = new MyStoresFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_stores, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/b07_final_project/StoreItemsFragment.java
@@ -0,0 +1,31 @@
+package com.example.b07_final_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StoreItemsFragment extends Fragment {
+
+    public StoreItemsFragment() {
+        super(R.layout.fragment_store_items);
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_store_items, container, false);
+    }
+}
