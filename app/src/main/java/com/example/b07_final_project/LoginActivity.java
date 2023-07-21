831a1c508427e130d8b53e1cd241b10e6e4fbd0b
Michael Zhou <michaelzhou20111@hotmail.com>
2023-07-21 19:23:11 -0400

    made some important clean up changes. also added a singleton class that stores the current user id which allows every activity to access the current user it

---
+++ app/src/main/java/com/example/b07_final_project/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.b07_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+    String userId = CurrentUserData.getInstance().getId();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void onClickLogin(View view){
+        //check for user id
+        CurrentUserData.getInstance().setId("this is my id");
+        Intent intent = new Intent(this, ItemActivity.class);
+        startActivity(intent);
+    }
+}
