ec5d65fefeb3fdf93163465176014e7597c81d80
IshtiaqueWalid <ishtiaque.walid@mail.utoronto.ca>
2023-07-24 03:46:48 -0400

    Added functionality to update database and added error text

--- app/src/main/java/com/example/b07_final_project/CreateStoreActivity.java
+++ app/src/main/java/com/example/b07_final_project/CreateStoreActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateStoreActivity extends AppCompatActivity {
     DatabaseReference db;
+    DatabaseReference ownerRef;
+    DatabaseReference storesRef;
+
+    String username = CurrentUserData.getInstance().getId();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +28,13 @@ public class CreateStoreActivity extends AppCompatActivity {
         db = FirebaseDatabase.getInstance("https://test-54768-default-rtdb.firebaseio.com/").getReference();
     }
 
+    private void showError(String errorText) {
+        // Replace the error text to display its message
+        ((TextView) findViewById(R.id.createStoreError)).setText(errorText);
+    }
+
     public void onClickCreateStore(View view) {
-        // When the Create store button is clicked...
+        // When the 'Create store' button is clicked...
 
         // Get user's input
         String storeName = ((EditText) findViewById(R.id.editTextStoreName))
@@ -29,6 +44,45 @@ public class CreateStoreActivity extends AppCompatActivity {
                 .getText()
                 .toString();
 
-        // TODO: Add the store to DB
+        // DB references for owner and stores
+        ownerRef = db.child("Owners").child(username);
+        storesRef = db.child("Stores");
+
+        // Check store among other stores and check if it's valid
+        // If it is, add it to the DB
+        storesRef.get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                // Error fetching data
+                showError("@string/create_store_data_error");
+                return;
+            }
+
+            // Get list of stores from DB
+            List<String> stores = new ArrayList<>();
+            for (DataSnapshot child : task.getResult().getChildren()) {
+                stores.add(child.getKey());
+            }
+            if (stores.contains(storeName)) {
+                // Store already exists
+                showError("@string/create_store_exists_error");
+                return;
+            }
+
+            // Check if store name is empty
+            if (storeName.equals("")) {
+                // Show the error
+                showError("@string/create_store_empty_error");
+                return;
+            }
+
+            // Add store to DB
+            DatabaseReference newStoreRef = storesRef.child(storeName);
+            newStoreRef.child("description").setValue(storeDescription);
+            // Add store key to owner
+            ownerRef.child("storeKey").child(storeName);
+
+            // TODO: Switch to the store owner's store page activity
+            showError("Success");
+        });
     }
 }
