ad9c2063524c434f65ec34d8333ea4d5e2c3de98
yong1le <ygle.he@gmail.com>
2023-07-24 21:00:16 -0400

    add image field in new item form

--- app/src/main/java/com/example/b07_final_project/NewItemActivity.java
+++ app/src/main/java/com/example/b07_final_project/NewItemActivity.java
@@ -1,18 +1,29 @@
 package com.example.b07_final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.b07_final_project.classes.Item;
+import com.example.b07_final_project.classes.Store;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewItemActivity extends AppCompatActivity {
-    String storeId = CurrentStoreData.getInstance().getId();
+//    String storeId = CurrentStoreData.getInstance().getId();
+    String storeId = "1234";
     FirebaseDatabase db = FirebaseDatabase.getInstance("https://testing-6c0f3-default-rtdb.firebaseio.com/");
 
     @Override
@@ -25,6 +36,7 @@ public class NewItemActivity extends AppCompatActivity {
         // Get all the needed input fields
         EditText nameInput = findViewById(R.id.itemNameInput);
         EditText priceInput = findViewById(R.id.itemPriceInput);
+        EditText imageInput = findViewById(R.id.itemImageInput);
         EditText descInput = findViewById(R.id.itemDescriptionInput);
 
         // This is for showing the success/error message in a Toast, so that it can disappear after
@@ -37,15 +49,43 @@ public class NewItemActivity extends AppCompatActivity {
             DatabaseReference newItem = db.getReference("Items").push();
             String itemKey = newItem.getKey();
 
-            // TODO: Discuss whether to use classes
-            newItem.child("itemName").setValue(nameInput.getText().toString());
-            newItem.child("price").setValue(priceInput.getText().toString());
-            newItem.child("description").setValue(descInput.getText().toString());
-            newItem.child("storeKey").setValue(storeId);
+            Item item = new Item(
+                    nameInput.getText().toString(),
+                    descInput.getText().toString(),
+                    Float.parseFloat(priceInput.getText().toString()),
+                    imageInput.getText().toString(),
+                    storeId
+            );
+            newItem.setValue(item);
 
             // This is needed to add the item in the Stores.id.items list
             DatabaseReference storeRef = db.getReference("Stores").child(storeId);
-            if (itemKey != null) storeRef.child("items").child(itemKey).setValue(nameInput.getText().toString());
+            storeRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        Store curStore = task.getResult().getValue(Store.class);
+                        // If items is not initialized yet
+                        if (curStore.getItems() == null) {
+                            ArrayList<String> items = new ArrayList<String>();
+                            items.add(itemKey);
+                            curStore.setItems(items);
+                        }
+                        // If it is, we just add on to the list
+                        else {
+                            List<String> items = curStore.getItems();
+                            // Even tho it is a List (Interface with no implementation). We can call
+                            // .add() because the list is always initialized as an ArrayList (see above).
+                            items.add(itemKey);
+                            curStore.setItems(items);
+                        }
+                        storeRef.setValue(curStore);
+                    }
+                }
+            });
+
+//            if (itemKey != null)
+//                storeRef.child("items").child(itemKey).setValue(nameInput.getText().toString());
 
             // Set text for snack bar
             successPrompt = Snackbar.make(view, "Successfully added item", 3000);
@@ -54,6 +94,7 @@ public class NewItemActivity extends AppCompatActivity {
             nameInput.setText("");
             priceInput.setText("");
             descInput.setText("");
+            imageInput.setText("");
         } catch (Exception e) {
             successPrompt = Snackbar.make(view, "Failed to add item", 3000);
         }
